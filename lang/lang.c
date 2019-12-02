4621c5432ec91c1c19b3312623011b6732a7d97e
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-12-02 20:20:23 +0900

    Add function

--- lang/lang.c
+++ lang/lang.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 static char *p;
+static char  func[26][100];
 
 __attribute__((noreturn)) static void error(char *fmt, ... ) {
     va_list ap;
@@ -14,21 +15,93 @@ __attribute__((noreturn)) static void error(char *fmt, ... ) {
     exit(1);
 }
 
-static int eval(int arg) {
+static void read_until(char c, char *buf) {
+    for (; *p != c; ++p, ++buf) {
+        *buf = *p;
+    }
+    ++p;
+    *buf = '\0';
+}
+
+static void skip() {
+    while (isspace(*p)) {
+        p++;
+    }
+}
+
+static void expect(char c) {
+    if (*p != c) {
+        error("%c expected: %s", c, p);
+    }
+    ++p;
+}
+
+static int eval(int *args);
+
+static int eval_string(char *code, int *args) {
+    char *orig = p;
+    p = code;
+    int val;
+    while (*p) {
+        val = eval(args);
+    }
+    p = orig;
+    return val;
+}
+
+static int eval(int *args) {
     while(*p) {
-        if(isspace(*p)) {
-            p++;
-            continue;
+        skip();
+
+        // Function parameter
+        if ('a' <= *p && *p <= 'z') {
+            return args[*p++ - 'a'];
         }
 
+        // Function definition
+        if ('A' <= *p && *p <= 'Z' && p[1] == '[') {
+            char name = *p;
+            p += 2;
+            read_until(']', func[name - 'A']);
+            return eval(args);
+        }
+
+        // 'P' print primitive
+        if (*p == 'P') {
+            ++p;
+            expect('(');
+            int val = eval(args);
+            expect(')');
+            printf("%d\n", val);
+            return val;
+        }
+
+        // Function application
+        if ('A' <= *p && *p <= 'Z' && p[1] == '(') {
+            int newargs[26];
+            char name = *p;
+            p += 2;
+
+            int i = 0;
+            for (skip(); *p != ')'; skip()) {
+                newargs[i++] = eval(args);
+            }
+
+            expect(')');
+
+            return eval_string(func[name - 'A'], newargs);
+        }
+
+        // Literal numbers
         if(isdigit(*p)) {
             return strtol(p, &p, 10);
         }
 
+        // Arithmetic operators
         if(strchr("+-*/", *p)) {
             int op = *p++;
-            int x  = eval();
-            int y  = eval();
+            int x  = eval(args);
+            int y  = eval(args);
             switch(op) {
                 case '+': return x + y;
                 case '-': return x - y;
@@ -49,7 +122,7 @@ int main(int argc, char **argv) {
     }
     p = argv[1];
     while(*p)
-        printf("%d\n", eval());
+        printf("%d\n", eval(0));
 
     return 0;
 }
--- lang/test.sh
+++ lang/test.sh
@@ -3,11 +3,11 @@
 gcc -o lang lang.c
 
 try() {
-    expected="$1"
-    actual=$(./lang "$2")
+    actual=$(./lang "$1")
+    expected="$2"
 
     if [ "$actual" = "$expected" ]; then
-        echo "$2 => $actual"
+        echo "$1 => $actual"
     else
         echo "$expected expected, but got $actual"
         exit 1
@@ -19,14 +19,28 @@ try 0 0
 try 99 99
 
 echo "=== arithmetic operators ==="
-try 2 "+ 1 1"
-try 46 "+ 12 34"
-try 22 "- 43 21"
-try -4 "- 1 5"
-try 15 "* 3 5"
-try 5 "/ 20 4"
-try 10 "+ + + 1 2 3 4"
-try 10 "+ 1 + 2 + 3 4"
-try 14 "+ 2 *4 3"
+try "+ 1 1" 2
+try "+ 12 34" 46
+try "- 43 21" 22
+try "- 1 5" -4
+try "* 3 5" 15
+try "/ 20 4" 5
+try "+ + + 1 2 3 4" 10
+try "+ 1 + 2 + 3 4" 10
+try "+ 2 *4 3" 14
+
+echo "=== functions ==="
+try "F[+ a a] F(1)" 2
+try "F[* a 2] F(5)" 10
+try "F[* a a] F(F(2))" 16
+try "F[* a a] F(F(F(2)))" 256
+try "F[* a b] F(3 5)" 15
+
+echo "=== build-in functions ==="
+try "P(5)" "5
+5"
+
+echo "=== fibonacci sequence ==="
+./lang "F[P(a) F(b + a b)] F(1 1)" | head -n 10
 
 echo OK
