a659373c246558f8d938d3722b2f9f52e78da8c2
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-05-12 13:25:45 +0900

    Added form

--- db/prototype/web_server/src/main.rs
+++ db/prototype/web_server/src/main.rs
@@ -15,13 +15,26 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let post = b"POST / HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(post) {
+        let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+        let request = String::from_utf8_lossy(&buffer[..]);
+
+        let contents = parse_request_line(&request);
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+        return;
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
@@ -35,4 +48,16 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+fn parse_request_line(request: &str) -> String {
+    let mut post = String::new();
+
+    for line in request.lines() {
+        if line.starts_with("sql=") {
+            post += line;
+        }
+    }
+
+    format!(r#"<html><head><meta charset="utf-8"><title>sql</title><body>{}</body></html>"#, post)
 }
