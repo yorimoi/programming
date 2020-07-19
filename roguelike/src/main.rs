39c0da93481ebb8d9d09503c7d8d1a129c0fbb83
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-07-19 12:22:37 +0900

    Added a part4

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -2,6 +2,7 @@ use std::cmp;
 
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::map::{FovAlgorithm, Map as FovMap};
 use rand::Rng;
 
 // actual size of the window
@@ -17,14 +18,21 @@ const ROOM_MAX_SIZE: i32 = 10;
 const ROOM_MIN_SIZE: i32 =  6;
 const MAX_ROOMS:     i32 = 30;
 
-const COLOR_DARK_WALL:   Color = Color { r:  0, g:  0, b: 100 };
-const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;  // default FOV algorithm
+const FOV_LIGHT_WALLS: bool = true;  // light walls or not
+const TORCH_RADIUS: i32 = 10;
+
+const COLOR_DARK_WALL:    Color = Color { r:   0, g:   0, b: 100 };
+const COLOR_LIGHT_WALL:   Color = Color { r: 130, g: 110, b:  50 };
+const COLOR_DARK_GROUND:  Color = Color { r:  50, g:  50, b: 150 };
+const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b:  50 };
 
 const LIMIT_FPS: i32 = 20;  // 20 frames-per-second maximum
 
 struct Tcod {
     root: Root,
     con: Offscreen,
+    fov: FovMap,
 }
 
 /// This is a generic object: the player, a monster, an item, the stairs...
@@ -60,6 +68,7 @@ impl Object {
 #[derive(Clone, Copy, Debug)]
 struct Tile {
     blocked: bool,
+    explored: bool,
     block_sight: bool,
 }
 
@@ -67,6 +76,7 @@ impl Tile {
     pub fn empty() -> Self {
         Tile {
             blocked: false,
+            explored: false,
             block_sight: false,
         }
     }
@@ -74,6 +84,7 @@ impl Tile {
     pub fn wall() -> Self {
         Tile {
             blocked: true,
+            explored: false,
             block_sight: true,
         }
     }
@@ -203,22 +214,44 @@ fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
     }
 }
 
-fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+fn render_all(tcod: &mut Tcod, game: &mut Game, objects: &[Object], fov_recompute: bool) {
+    if fov_recompute {
+        // recompute FOV if needed (the player moved or something)
+        let player = &objects[0];
+        tcod.fov.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
+    }
+
     // go through all tiles, and set their background color
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
+            let visible = tcod.fov.is_in_fov(x, y);
             let wall = game.map[x as usize][y as usize].block_sight;
-            if wall {
-                tcod.con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                tcod.con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            let color = match (visible, wall) {
+                // outside of field of view:
+                (false, true)  => COLOR_DARK_WALL,
+                (false, false) => COLOR_DARK_GROUND,
+                // inside fov:
+                (true, true)   => COLOR_LIGHT_WALL,
+                (true, false)  => COLOR_LIGHT_GROUND,
+            };
+
+            let explored = &mut game.map[x as usize][y as usize].explored;
+            if visible {
+                // since it's visible, explore it
+                *explored = true;
+            }
+            if *explored {
+                // show explored tiles only (any visible tile is explored already)
+                tcod.con.set_char_background(x, y, color, BackgroundFlag::Set);
             }
         }
     }
 
     // draw all objects in the list
     for object in objects {
-        object.draw(&mut tcod.con);
+        if tcod.fov.is_in_fov(object.x, object.y) {
+            object.draw(&mut tcod.con);
+        }
     }
 
     // blit the contents of "con" to the root console and present it
@@ -271,9 +304,11 @@ fn main() {
         .title("roguelike")
         .init();
 
-    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
-
-    let mut tcod = Tcod { root, con };
+    let mut tcod = Tcod {
+        root,
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT),
+        fov: FovMap::new(MAP_WIDTH, MAP_HEIGHT),
+    };
 
     // create object representing the player
     let player = Object::new(0, 0, '@', WHITE);
@@ -284,22 +319,39 @@ fn main() {
     // the list of objects with those two
     let mut objects = [player, npc];
 
-    let game = Game {
+    let mut game = Game {
         // generate map (at this point it's not drawn to the screen)
         map: make_map(&mut objects[0]),
     };
+
+    // populate the FOV map, according to the generated map
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            tcod.fov.set(
+                x,
+                y,
+                !game.map[x as usize][y as usize].block_sight,
+                !game.map[x as usize][y as usize].blocked,
+            )
+        }
+    }
+
+    // force FOV "recompute" first time through the game loop
+    let mut previous_player_position = (-1, -1);
 
     while !tcod.root.window_closed() {
         // clear the screen of the previous frame
         tcod.con.clear();
 
         // render the screen
-        render_all(&mut tcod, &game, &objects);
+        let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+        render_all(&mut tcod, &mut game, &objects, fov_recompute);
 
         tcod.root.flush();
 
         // handle keys and exit game if needed
         let player = &mut objects[0];
+        previous_player_position = (player.x, player.y);
         let exit = handle_keys(&mut tcod, &game, player);
         if exit {
             break;
