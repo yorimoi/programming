c8628e1d3f2badf017a50b4e0fcd8ee69257788e
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-07-26 14:23:58 +0900

    Supported a color

--- tetris/rust_tetris2/src/main.rs
+++ tetris/rust_tetris2/src/main.rs
@@ -9,9 +9,40 @@ const FIELD_HEIGHT: usize = 20 + 1 + 2;  // field + wall + sentinel
 
 const MINO_TYPE_LENGTH: usize = 7;  // I O S Z J L T
 
+#[derive(Clone, Copy, PartialEq)]
+enum MinoType {
+    NONE = 0,
+    WALL,
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
+}
+
+impl From<usize> for MinoType {
+    fn from(i: usize) -> Self {
+        match i {
+            0 => MinoType::NONE,
+            1 => MinoType::WALL,
+            2 => MinoType::I,
+            3 => MinoType::O,
+            4 => MinoType::S,
+            5 => MinoType::Z,
+            6 => MinoType::J,
+            7 => MinoType::L,
+            8 => MinoType::T,
+            _ => unreachable!(),
+        }
+    }
+}
+
 struct Game {
-    field: [[i32; FIELD_WIDTH]; FIELD_HEIGHT],
+    field: [[MinoType; FIELD_WIDTH]; FIELD_HEIGHT],
     shape: [i32; 4],
+    mino_type: MinoType,
     x: i32,
     y: i32,
 }
@@ -19,8 +50,9 @@ struct Game {
 impl Game {
     fn new() -> Self {
         let mut game = Game {
-            field: [[0; FIELD_WIDTH]; FIELD_HEIGHT],
+            field: [[MinoType::NONE; FIELD_WIDTH]; FIELD_HEIGHT],
             shape: [0; 4],
+            mino_type: MinoType::WALL,
             x: 0,
             y: 0,
         };
@@ -28,11 +60,11 @@ impl Game {
         game.init_mino();
 
         for y in 0..FIELD_HEIGHT {
-            game.field[y][1]             = 1;
-            game.field[y][FIELD_WIDTH-2] = 1;
+            game.field[y][1]             = MinoType::WALL;
+            game.field[y][FIELD_WIDTH-2] = MinoType::WALL;
         }
         for x in 0..FIELD_WIDTH {
-            game.field[FIELD_HEIGHT-2][x] = 1;
+            game.field[FIELD_HEIGHT-2][x] = MinoType::WALL;
         }
 
         game
@@ -59,28 +91,40 @@ impl Game {
         self.shape[2] = MINOS[rnd][1];
         self.shape[3] = 0;
 
+        self.mino_type = MinoType::from(rnd + MinoType::I as usize);
+
         self.x = 5;
         self.y = 0;
     }
 
     fn draw(&self) {
+        static COLORS: &[&str] = &[
+            "\x1b[48;2;000;000;000m  ",  // NONE
+            "\x1b[48;2;127;127;127m__",  // WALL
+            "\x1b[48;2;000;255;255m__",  // I
+            "\x1b[48;2;255;255;000m__",  // O
+            "\x1b[48;2;000;255;000m__",  // S
+            "\x1b[48;2;255;000;000m__",  // Z
+            "\x1b[48;2;000;000;255m__",  // J
+            "\x1b[48;2;255;127;000m__",  // L
+            "\x1b[48;2;127;000;255m__",  // T
+        ];
         let mut field_buf = self.field;
-        let mut output = String::with_capacity(FIELD_WIDTH * FIELD_HEIGHT * 2);
+        let mut output = String::new();
 
         for y in 0..4 {
             for x in 0..4 {
-                field_buf[y + self.y as usize][x + self.x as usize] |= self.shape[y] >> (3-x) & 1;
+                if self.shape[y] >> (3-x) & 1 == 1 {
+                    field_buf[y + self.y as usize][x + self.x as usize] = self.mino_type;
+                }
             }
         }
 
-        output.push_str("\x1b[H");
+        output.push_str("\x1b[H\n");
         for line in field_buf.iter().take(FIELD_HEIGHT-1).skip(1) {
+            output.push_str("\x1b[0m  ");
             for col in line.iter().take(FIELD_WIDTH-1).skip(1) {
-                if *col == 0 {
-                    output.push_str(" .");
-                } else {
-                    output.push_str("[]");
-                }
+                output.push_str(COLORS[*col as usize]);
             }
             output.push('\n');
         }
@@ -95,12 +139,49 @@ impl Game {
                 if x+gx < 0 || FIELD_WIDTH as i32 <= x+gx || y+gy < 0 || FIELD_HEIGHT as i32 <= y+gy {
                     continue;
                 }
-                if self.field[(y+gy) as usize][(x+gx) as usize] & (shape[y as usize] >> (3-x) & 1) == 1 {
+                if self.field[(y+gy) as usize][(x+gx) as usize] != MinoType::NONE && shape[y as usize] >> (3-x) & 1 == 1 {
                     return true;
                 }
             }
         }
         false
+    }
+
+    fn fix2field(&mut self) {
+        let gy = self.y as usize;
+        let gx = self.x as usize;
+        for y in 0..4 {
+            for x in 0..4 {
+                if self.shape[y] >> (3-x) & 1 == 1 {
+                    self.field[y+gy][x+gx] = self.mino_type;
+                }
+            }
+        }
+    }
+
+    fn erase_line(&mut self) {
+        for y in 1..FIELD_HEIGHT-2 {
+            let mut can_erase = true;
+            for x in 2..=FIELD_WIDTH-3 {
+                if self.field[y][x] == MinoType::NONE {
+                    can_erase = false;
+                    break;
+                }
+            }
+            if can_erase {
+                for y2 in (1..y+1).rev() {
+                    self.field[y2] = self.field[y2-1];
+                }
+            }
+        }
+    }
+
+    fn check_gameover(&self) {
+        if self.is_collision(0, 0, &self.shape) {
+            self.draw();
+            println!("\x1b[0m         GAME OVER\x1b[?25h");
+            std::process::exit(0);
+        }
     }
 }
 
@@ -118,39 +199,16 @@ fn gameloop(game: &mut Arc<Mutex<Game>>) {
                     game.y += 1;
                 } else {
                     // Fix to field
-                    let gy = game.y as usize;
-                    let gx = game.x as usize;
-                    for y in 0..4 {
-                        for x in 0..4 {
-                            game.field[y+gy][x+gx] |= game.shape[y] >> (3-x) & 1;
-                        }
-                    }
+                    game.fix2field();
 
                     // Erase line
-                    for y in 1..FIELD_HEIGHT-2 {
-                        let mut can_erase = true;
-                        for x in 2..=FIELD_WIDTH-3 {
-                            if game.field[y][x] == 0 {
-                                can_erase = false;
-                                break;
-                            }
-                        }
-                        if can_erase {
-                            for y2 in (1..y+1).rev() {
-                                game.field[y2] = game.field[y2-1];
-                            }
-                        }
-                    }
+                    game.erase_line();
 
                     // Spawn mino
                     game.init_mino();
 
                     // Check gameover
-                    if game.is_collision(0, 0, &game.shape) {
-                        game.draw();
-                        println!("        GAME OVER\x1b[?25h");
-                        std::process::exit(0);
-                    }
+                    game.check_gameover();
                 }
             }
         });
@@ -210,12 +268,12 @@ fn gameloop(game: &mut Arc<Mutex<Game>>) {
 }
 
 fn main() {
-    let game = Game::new();
-
+    // Clear screen, hide cursor
     println!("\x1b[2J\x1b[H\x1b[?25l");
 
-    gameloop(&mut Arc::new(Mutex::new(game)));
+    gameloop(&mut Arc::new(Mutex::new(Game::new())));
 
-    println!("\x1b[?25h");
+    // Reset escape, display cursor
+    println!("\x1b[0m\x1b[?25h");
 }
 
