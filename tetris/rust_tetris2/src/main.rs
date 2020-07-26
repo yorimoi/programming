27349599b72884cf219f6ccd89a9b48238294b83
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-07-26 22:34:56 +0900

    Updated a rust_tetris2     * Supported a hold     * Updated a next mino     * Updated a draw

--- tetris/rust_tetris2/src/main.rs
+++ tetris/rust_tetris2/src/main.rs
@@ -1,13 +1,26 @@
 use std::{thread, time};
 use std::sync::{Arc, Mutex};
 
-use rand::Rng;
+use rand::seq::SliceRandom;
 use getch::Getch;
 
 const FIELD_WIDTH:  usize = 10 + 2 + 2;  // field + wall + sentinel
 const FIELD_HEIGHT: usize = 20 + 1 + 2;  // field + wall + sentinel
 
 const MINO_TYPE_LENGTH: usize = 7;  // I O S Z J L T
+
+/*   8421
+ * 0 0000
+ * 1 0000 */
+static MINOS: [[i32; 2]; 7] = [
+    [ 0, 15],  // I
+    [ 6,  6],  // O
+    [ 6, 12],  // S
+    [12,  6],  // Z
+    [ 8, 14],  // J
+    [ 2, 14],  // L
+    [ 4, 14],  // T
+];
 
 #[derive(Clone, Copy, PartialEq)]
 enum MinoType {
@@ -41,8 +54,13 @@ impl From<usize> for MinoType {
 
 struct Game {
     field: [[MinoType; FIELD_WIDTH]; FIELD_HEIGHT],
+    mino_stack_main: [MinoType; MINO_TYPE_LENGTH],
+    mino_stack_sub:  [MinoType; MINO_TYPE_LENGTH],
+    mino_stack_index: usize,
     shape: [i32; 4],
     mino_type: MinoType,
+    hold: Option<MinoType>,
+    is_hold: bool,
     x: i32,
     y: i32,
 }
@@ -51,14 +69,24 @@ impl Game {
     fn new() -> Self {
         let mut game = Game {
             field: [[MinoType::NONE; FIELD_WIDTH]; FIELD_HEIGHT],
+            mino_stack_main: [MinoType::NONE; MINO_TYPE_LENGTH],
+            mino_stack_sub:  [MinoType::NONE; MINO_TYPE_LENGTH],
+            mino_stack_index: 0,
             shape: [0; 4],
             mino_type: MinoType::WALL,
+            hold: None,
+            is_hold: false,
             x: 0,
             y: 0,
         };
 
-        game.init_mino();
+        // Init mino stakcs
+        init_miso_stack(&mut game.mino_stack_main);
+        init_miso_stack(&mut game.mino_stack_sub);
 
+        game.next_mino();
+
+        // Create wall
         for y in 0..FIELD_HEIGHT {
             game.field[y][1]             = MinoType::WALL;
             game.field[y][FIELD_WIDTH-2] = MinoType::WALL;
@@ -70,31 +98,22 @@ impl Game {
         game
     }
 
-    fn init_mino(&mut self) {
-        /*   8421
-         * 0 0000
-         * 1 0000 */
-        static MINOS: [[i32; 2]; 7] = [
-            [15,  0],  // I
-            [ 6,  6],  // O
-            [ 6, 12],  // S
-            [12,  6],  // Z
-            [14,  2],  // J
-            [ 2, 14],  // L
-            [ 4, 14],  // T
-        ];
+    fn next_mino(&mut self) {
+        if MINO_TYPE_LENGTH <= self.mino_stack_index {
+            self.mino_stack_index = 0;
+            self.mino_stack_main = self.mino_stack_sub;
+            init_miso_stack(&mut self.mino_stack_sub);
+        }
 
-        let rnd = rand::thread_rng().gen_range(0, MINO_TYPE_LENGTH);
+        self.mino_type = self.mino_stack_main[self.mino_stack_index];
+        self.mino_stack_index += 1;
 
-        self.shape[0] = 0;
-        self.shape[1] = MINOS[rnd][0];
-        self.shape[2] = MINOS[rnd][1];
-        self.shape[3] = 0;
-
-        self.mino_type = MinoType::from(rnd + MinoType::I as usize);
+        set_mino_shape(&mut self.shape, self.mino_type);
 
         self.x = 5;
         self.y = 0;
+
+        self.is_hold = false;
     }
 
     fn draw(&self) {
@@ -120,7 +139,7 @@ impl Game {
             }
         }
 
-        output.push_str("\x1b[H\n");
+        output.push_str("\x1b[H\n\n\n");
         for line in field_buf.iter().take(FIELD_HEIGHT-1).skip(1) {
             output.push_str("\x1b[0m  ");
             for col in line.iter().take(FIELD_WIDTH-1).skip(1) {
@@ -128,6 +147,56 @@ impl Game {
             }
             output.push('\n');
         }
+        output.push_str("\x1b[0m");
+
+        // Display hold
+        if let Some(hold) = self.hold {
+            output.push_str(&format!("\x1b[{};{}HHOLD", 1, FIELD_WIDTH * 2));
+            let mino = MINOS[hold as usize - MinoType::I as usize];
+            for y in 0..2 {
+                output.push_str(&format!("\x1b[{};{}H", 3 + y, FIELD_WIDTH * 2));
+                for x in (0..4).rev() {
+                    if mino[y] >> x & 1 == 1 {
+                        output.push_str(&format!("{}\x1b[0m", COLORS[hold as usize]));
+                    } else {
+                        output.push_str("  ");
+                    }
+                }
+            }
+        }
+
+        // Display nexts
+        output.push_str(&format!("\x1b[{};{}HNEXT", 6, FIELD_WIDTH * 2));
+        let mut next_mino_count = 0;
+        for i in self.mino_stack_index..MINO_TYPE_LENGTH {
+            let mino = MINOS[self.mino_stack_main[i] as usize - MinoType::I as usize];
+            for y in 0..2 {
+                output.push_str(&format!("\x1b[{};{}H", 8 + y + next_mino_count * 3, FIELD_WIDTH * 2));
+                for x in (0..4).rev() {
+                    if mino[y] >> x & 1 == 1 {
+                        output.push_str(&format!("{}\x1b[0m", COLORS[self.mino_stack_main[i] as usize]));
+                    } else {
+                        output.push_str("  ");
+                    }
+                }
+            }
+            next_mino_count += 1;
+        }
+        for i in 0..self.mino_stack_index-1 {
+            let mino = MINOS[self.mino_stack_sub[i] as usize - MinoType::I as usize];
+            for y in 0..2 {
+                output.push_str(&format!("\x1b[{};{}H", 8 + y + next_mino_count * 3, FIELD_WIDTH * 2));
+                for x in (0..4).rev() {
+                    if mino[y] >> x & 1 == 1 {
+                        output.push_str(&format!("{}\x1b[0m", COLORS[self.mino_stack_sub[i] as usize]));
+                    } else {
+                        output.push_str("  ");
+                    }
+                }
+            }
+            next_mino_count += 1;
+        }
+
         println!("{}", output);
     }
 
@@ -183,6 +252,46 @@ impl Game {
             std::process::exit(0);
         }
     }
+
+    fn hold(&mut self) {
+        if self.is_hold {
+            // Do nothing
+            return;
+        }
+
+        if let Some(mut hold) = self.hold {
+            std::mem::swap(&mut hold, &mut self.mino_type);
+            set_mino_shape(&mut self.shape, self.mino_type);
+            self.hold = Some(hold);
+            self.x = 5;
+            self.y = 0;
+        } else {
+            self.hold = Some(self.mino_type);
+            self.next_mino();
+        }
+        self.is_hold = true;
+    }
+}
+
+fn set_mino_shape(shape: &mut [i32; 4], mino_type: MinoType) {
+    shape[0] = 0;
+    shape[1] = MINOS[mino_type as usize - MinoType::I as usize][0];
+    shape[2] = MINOS[mino_type as usize - MinoType::I as usize][1];
+    shape[3] = 0;
+}
+
+fn init_miso_stack(stack: &mut [MinoType; MINO_TYPE_LENGTH]) {
+    let mut new_stack = [
+        MinoType::I,
+        MinoType::O,
+        MinoType::S,
+        MinoType::Z,
+        MinoType::J,
+        MinoType::L,
+        MinoType::T,
+    ];
+    new_stack.shuffle(&mut rand::thread_rng());
+    *stack = new_stack;
 }
 
 fn gameloop(game: &mut Arc<Mutex<Game>>) {
@@ -204,8 +313,8 @@ fn gameloop(game: &mut Arc<Mutex<Game>>) {
                     // Erase line
                     game.erase_line();
 
-                    // Spawn mino
-                    game.init_mino();
+                    // Spawn next mino
+                    game.next_mino();
 
                     // Check gameover
                     game.check_gameover();
@@ -248,8 +357,21 @@ fn gameloop(game: &mut Arc<Mutex<Game>>) {
                     game.y += 1;
                 }
             }
+            // Left rotation
+            Ok(b'z') => {
+                let mut game = game.lock().unwrap();
+                let mut s: [i32; 4] = [0; 4];
+                for y in 0..4 {
+                    for x in 0..4 {
+                        s[x] |= (game.shape[4-1-y]>>x & 1) << y;
+                    }
+                }
+                if !game.is_collision(0, 0, &s) {
+                    game.shape = s;
+                }
+            }
             // Right rotation
-            Ok(b' ') => {
+            Ok(b'x') => {
                 let mut game = game.lock().unwrap();
                 let mut s: [i32; 4] = [0; 4];
                 for y in 0..4 {
@@ -260,6 +382,10 @@ fn gameloop(game: &mut Arc<Mutex<Game>>) {
                 if !game.is_collision(0, 0, &s) {
                     game.shape = s;
                 }
+            }
+            // Hold
+            Ok(b' ') => {
+                game.lock().unwrap().hold();
             }
             _  => (),
         }
